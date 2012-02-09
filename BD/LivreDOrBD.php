52d7fe4b7b1b15f881e99812d6ad32e6bab6feb9
Olivier MANDIN <olivier.mandin.44@gmail.com>
2012-02-09 14:42:47 +0100

    livreOr accepter signature OK

--- BD/LivreDOrBD.php
+++ BD/LivreDOrBD.php
@@ -1,37 +1,68 @@
 <?php
-include_once('BD.php');
-include_once('../METIER/LivreDOr.php');
-
-/** getAllLivreDOr()
- *  Renvoie un tableau contenant tous les commentaires du livre d or sous forme d objets LivreDOr
- *  @return LivreDOr $tableauDeLivreDOr[] : un tableau contenant tous les LivreDOr existants
- */
-
-function getAllLivreDOr()
-{
-	$bd = new BD();
-	$tableauDeLivreDOr = array();
-	
-	try
-	{
-		$bd->connexion();											// Connexion a la BD
-		$connexion = $bd->getConnexion();
-		$resultQuery = $connexion->query("SELECT * FROM LIVRE_OR")->fetchAll();	// Execution de la requete
-		
-		foreach($resultQuery as $row)
-		{
-			$livre = new LivreDOr($row['id_post'], $row['posteur_post'], $row['mail_post'], $row['date_post'], $row['message_post']);
-			array_push($tableauDeLivreDOr, $livre);
-		}
-	}
-	catch(PDOException $e)
-	{
-		
-	}
-	$bd->deconnexion();	// Deconnexion de la BD
-	
-	return $tableauDeLivreDOr;
-}
-
+    include_once('BD.php');
+    include_once('../METIER/LivreDOr.php');
     
-?>
+    /** getAllLivreDOr()
+     *  Renvoie un tableau contenant tous les commentaires du livre d or sous forme d objets LivreDOr
+     *  @return LivreDOr $tableauDeLivreDOr[] : un tableau contenant tous les LivreDOr existants
+     */
+    
+    function getAllLivreDOr()
+    {
+        $bd = new BD();
+        $tableauDeLivreDOr = array();
+        
+        try
+        {
+            $bd->connexion();											// Connexion a la BD
+            $connexion = $bd->getConnexion();
+            $resultQuery = $connexion->query("SELECT * FROM LIVRE_OR")->fetchAll();	// Execution de la requete
+            
+            foreach($resultQuery as $row)
+            {
+                $livre = new LivreDOr($row['id_post'], $row['posteur_post'], $row['mail_post'], $row['date_post'], $row['message_post']);
+                array_push($tableauDeLivreDOr, $livre);
+            }
+        }
+        catch(PDOException $e)
+        {
+            
+        }
+        $bd->deconnexion();	// Deconnexion de la BD
+        
+        return $tableauDeLivreDOr;
+    }
+    
+    
+    
+    /** function getAllAcceptedLivreDOr()
+     * Liste l'ensemble des signatures acceptées dans un tableau
+     * @return LivreDOr $tableauDeLivreDOr[] : un tabeau contenant tous les LivreDOr listés
+     **/
+    function getAllAcceptedLivreDOr()
+    {
+        $bd = new BD();
+        $tableauDeLivreDOr = array();
+        
+        try
+        {
+            $bd->connexion();											// Connexion a la BD
+            $connexion = $bd->getConnexion();
+            $resultQuery = $connexion->query("SELECT * FROM LIVRE_OR WHERE accept_post = 1 ORDER BY date_post DESC")->fetchAll();	// Execution de la requete
+            
+            foreach($resultQuery as $row)
+            {
+                $livre = new LivreDOr($row['id_post'], $row['posteur_post'], $row['mail_post'], $row['date_post'], $row['message_post']);
+                array_push($tableauDeLivreDOr, $livre);
+            }
+        }
+        catch(PDOException $e)
+        {
+            
+        }
+        $bd->deconnexion();	// Deconnexion de la BD
+        
+        return $tableauDeLivreDOr;
+    }
+    
+    ?>
--- PRESENTATION/livreDOr.php
+++ PRESENTATION/livreDOr.php
@@ -2,9 +2,9 @@
 	session_start();
 	require_once('../BD/LivreDOrBD.php');
     require_once('../BD/LangueParser.php');
-	$tabComment = getAllLivreDOr();	// Creation de la liste des commentaires a afficher
+	$tabComment = getAllAcceptedLivreDOr();	// Creation de la liste des commentaires a afficher
     $parserLangue = new LangueParser();
-?>
+    ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -16,86 +16,86 @@
 <link rel="stylesheet" href="Style/livreDOr.css" type="text/css">
 <script language="javascript" src="JavaScript/checkLivreOr.js"></script>
 <title>
-	<?php
-		echo( $parserLangue->getWord('LivreOr')->getTraduction() );
+<?php
+    echo( $parserLangue->getWord('LivreOr')->getTraduction() );
     ?>
 </title>
 </head>
 
 <body>
-	<div id='page'>
-		<?php 
-			include_once('header.php');
-		?>
-	
-		<div id='global'>
-			<div id='left'>
-				<?php 
-					include_once('EXPLORER_CATEGORIES/explorer.php');
-				?>
-			</div>
-			<div id='right'>
-				<ul id='fil'>
-					<li>fil d'ariane</li>
-				</ul>
-				<div id='content'>
-					
-					<div id='titre'>
-						<h1>
-                            <?php
-                                echo( $parserLangue->getWord('LivreOr')->getTraduction() );
-                            ?>
-                        </h1>
-					</div>
-					<div id='signatures'>
-						<?php
-						// Affichage des commentaires
-						foreach($tabComment as $com)
-						{
-							echo("<table id='signature_".$com->getId()."' class='signature'>");
-							echo("<tr>");
-							echo("<td>".$com->getPosteur()."</td><td>".$com->getDate()."</td>");
-							echo("</tr>");
-							echo("<tr>");
-							echo("<td colspan='2'>".$com->getMessage()."</td>");
-							echo("</tr>");
-							echo("</table>");
-						}
-						?>
-					</div>
-					<div id='ajoutSignature'>
-                    <?php
-                        if(isset($_GET['emptyInput']) && $_GET['emptyInput'] == 'true')
-                        {
-                            echo("<p>Tous les champs doivent être remplis !</p>");
-                        }
-                        if(isset($_GET['mailSyntax']) && $_GET['mailSyntax'] == 'false')
-                        {
-                            echo("<p>L'adresse mail doit être valide !</p>");
-                        }
-                        if(isset($_GET['signAttempt']) && $_GET['signAttempt'] == 'true')
-                        {
-                            echo("<p>Signature ajoutée avec succès !</p>");
-                        }
-                        if(isset($_GET['signAttempt']) && $_GET['signAttempt'] == 'false')
-                        {
-                            echo("<p>Echec de l'ajout (problème serveur) !</p>");
-                        }
-                        
-                    ?>
-						<form name='signer' action='../BD/AjoutSignature.php' method="post" onsubmit="return check()">
-							Pseudo<input type="text" name='pseudo'/>Mail<input type="text" name='mail'/>
-							Message<textarea name='message'></textarea>
-							<input type="submit" id='signer' value='Signez'/>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php
-			include_once('footer.php');
-		?>
-	</div>
-		
+<div id='page'>
+<?php 
+    include_once('header.php');
+    ?>
+
+<div id='global'>
+<div id='left'>
+<?php 
+    include_once('EXPLORER_CATEGORIES/explorer.php');
+    ?>
+</div>
+<div id='right'>
+<ul id='fil'>
+<li>fil d'ariane</li>
+</ul>
+<div id='content'>
+
+<div id='titre'>
+<h1>
+<?php
+    echo( $parserLangue->getWord('LivreOr')->getTraduction() );
+    ?>
+</h1>
+</div>
+<div id='signatures'>
+<?php
+    // Affichage des commentaires
+    foreach($tabComment as $com)
+    {
+        echo("<table id='signature_".$com->getId()."' class='signature'>");
+        echo("<tr>");
+        echo("<td>".$com->getPosteur()."</td><td>".$com->getDate()."</td>");
+        echo("</tr>");
+        echo("<tr>");
+        echo("<td colspan='2'>".$com->getMessage()."</td>");
+        echo("</tr>");
+        echo("</table>");
+    }
+    ?>
+</div>
+<div id='ajoutSignature'>
+<?php
+    if(isset($_GET['emptyInput']) && $_GET['emptyInput'] == 'true')
+    {
+        echo("<p>Tous les champs doivent être remplis !</p>");
+    }
+    if(isset($_GET['mailSyntax']) && $_GET['mailSyntax'] == 'false')
+    {
+        echo("<p>L'adresse mail doit être valide !</p>");
+    }
+    if(isset($_GET['signAttempt']) && $_GET['signAttempt'] == 'true')
+    {
+        echo("<p>Signature ajoutée avec succès ! En attente d'approbation.</p>");
+    }
+    if(isset($_GET['signAttempt']) && $_GET['signAttempt'] == 'false')
+    {
+        echo("<p>Echec de l'ajout (problème serveur) !</p>");
+    }
+    
+    ?>
+<form name='signer' action='../BD/AjoutSignature.php' method="post" onsubmit="return check()">
+Pseudo<input type="text" name='pseudo'/>Mail<input type="text" name='mail'/>
+Message<textarea name='message'></textarea>
+<input type="submit" id='signer' value='Signez'/>
+</form>
+</div>
+</div>
+</div>
+</div>
+<?php
+    include_once('footer.php');
+    ?>
+</div>
+
 </body>
 </html>
