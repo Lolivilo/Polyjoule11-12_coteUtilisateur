c1b3391bd96a5364a18e75cc0e64ff3350850d09
Antonin Biret <haprock@gmail.com>
2012-04-10 19:16:08 +0200

    correction ajax album photo

--- PRESENTATION/JavaScript/album_photo.js
+++ PRESENTATION/JavaScript/album_photo.js
@@ -21,31 +21,3 @@ jQuery().ready(function(){$("#mycarousel li img").click(
 	}
 );
 });
-/* */
-
-/*
-
-
-$.post("localhost:8888/PRESENTATION/script_ajax_album_photo.php",
-                                                         {id_photo:id__photo,id_album:id__album},
-                                                           function(data){
-                                                           alert(data);
-                                                                /*if(data.success == 'yes')
-                                                                {
-                                                                	 alert('success');
-                                                                    $("#corps #photo > img#mainPhoto").attr('src',data.url);
-                                                                    $("#corps #photo > p").html(data.commentaire);
-                                                                    $("#corps > h4").html(data.titre);
-                                                         
-                                                                }
-                                                                else
-                                                                {
-                                                                	 alert('echec');
-                                                                     $("#corps #photo > p").html(data.texte);
-                                                                }
-                                                               // $("#loader").fadeOut();
-                                                         
-                                                         },"json");
-                                                         
-                                                         
-                                                        */
